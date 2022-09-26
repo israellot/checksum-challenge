f8f57a8e1e73a83ec20317ab7a168138b2d21b1c
Israel Lot <israel.lot@gmail.com>
2022-09-26 17:04:20 -0300

    adjust pro

--- src/ChecksumChallenge/Checksum/Pro.cs
+++ src/ChecksumChallenge/Checksum/Pro.cs
@@ -8,64 +8,57 @@ namespace ChecksumChallenge;
 
 internal partial class Checksum
 {
-    public static uint ChecksumPro(ReadOnlySpan<byte> arr)
+public static uint ChecksumPro(ReadOnlySpan<byte> arr)
+{
+    if (arr.Length == 0) return 0;
+
+    uint sum0 = 0, sum1 = 0, sum2 = 0, sum3 = 0;
+    int z = 0;
+
+    int rem = arr.Length % 16;
+    var limit = arr.Length - rem;
+
+    while (z < limit)
     {
-        if (arr.Length == 0) return 0;
+        sum0 += (uint)(arr[z + 0] + arr[z + 4] + arr[z + 8] + arr[z + 12]);
+        sum1 += (uint)(arr[z + 1] + arr[z + 5] + arr[z + 9] + arr[z + 13]);
+        sum2 += (uint)(arr[z + 2] + arr[z + 6] + arr[z + 10] + arr[z + 14]);
+        sum3 += (uint)(arr[z + 3] + arr[z + 7] + arr[z + 11] + arr[z + 15]);
 
-        uint sum0 = 0, sum1 = 0, sum2 = 0, sum3 = 0;
-        int z = 0;
-
-        int rem = arr.Length % 32;
-        var limit = arr.Length - rem;
-
-        while (z < limit)
-        {
-            sum0 += (uint)(arr[z + 0] + arr[z + 4] + arr[z + 8] + arr[z + 12]
-                    + arr[z + 16] + arr[z + 20] + arr[z + 24] + arr[z + 28]);
-
-            sum1 += (uint)(arr[z + 1] + arr[z + 5] + arr[z + 9] + arr[z + 13]
-                    + arr[z + 17] + arr[z + 21] + arr[z + 25] + arr[z + 29]);
-
-            sum2 += (uint)(arr[z + 2] + arr[z + 6] + arr[z + 10] + arr[z + 14]
-                    + arr[z + 18] + arr[z + 22] + arr[z + 26] + arr[z + 30]);
-
-            sum3 += (uint)(arr[z + 3] + arr[z + 7] + arr[z + 11] + arr[z + 15]
-                    + arr[z + 19] + arr[z + 23] + arr[z + 27] + arr[z + 31]);
-
-            z += 32;
-        }
-
-        limit = arr.Length - 4;
-        while (z <= limit)
-        {
-            sum0 += arr[z + 0];
-            sum1 += arr[z + 1];
-            sum2 += arr[z + 2];
-            sum3 += arr[z + 3];
-            z += 4;
-        }
-
-        var sum = sum3 + (sum2 << 8) + (sum1 << 16) + (sum0 << 24);
-
-        rem = arr.Length - z;
-        switch (rem & 3)
-        {
-            case 3:
-                sum += (uint)(arr[z + 2]) << 8;
-                sum += (uint)(arr[z + 1]) << 16;
-                sum += (uint)(arr[z + 0]) << 24;
-                break;
-            case 2:
-                sum += (uint)(arr[z + 1]) << 16;
-                sum += (uint)(arr[z + 0]) << 24;
-                break;
-            case 1:
-                sum += (uint)(arr[z + 0]) << 24;
-                break;
-        }
-
-        return sum;
+        z += 16;
     }
+
+    limit = arr.Length - 4;
+    while (z <= limit)
+    {
+        sum0 += arr[z + 0];
+        sum1 += arr[z + 1];
+        sum2 += arr[z + 2];
+        sum3 += arr[z + 3];
+        z += 4;
+    }
+
+    var sum = sum3 + (sum2 << 8) + (sum1 << 16) + (sum0 << 24);
+
+    rem = arr.Length - z;
+    switch (rem & 3)
+    {
+        case 3:
+            sum += (uint)(arr[z + 2]) << 8;
+            sum += (uint)(arr[z + 1]) << 16;
+            sum += (uint)(arr[z + 0]) << 24;
+            break;
+        case 2:
+            sum += (uint)(arr[z + 1]) << 16;
+            sum += (uint)(arr[z + 0]) << 24;
+            break;
+        case 1:
+            sum += (uint)(arr[z + 0]) << 24;
+            break;
+    }
+
+    return sum;
+}
 
 
 
