1d0bd1abe928429ab2c6afa3f0744601283e7851
TheThief <positronthief@gmail.com>
2022-10-13 11:09:17 +0100

    Interested to see what the optimiser makes of this

---
+++ src/ChecksumChallenge/Checksum/Junior2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChecksumChallenge;
+
+internal partial class Checksum
+{
+    public static uint ChecksumJunior2(ReadOnlySpan<byte> arr)
+    {
+        if (arr.Length == 0) return 0;
+
+        uint sum = 0;
+
+        for (var i = 0; i < arr.Length; i++)
+        {
+            switch (i % 4)
+            {
+                case 0: sum += arr[i] << 24; break;
+                case 1: sum += arr[i] << 16; break;
+                case 2: sum += arr[i] <<  8; break;
+                case 3: sum += arr[i] <<  0; break;
+            }
+        }
+
+        return sum;
+    }
+}
